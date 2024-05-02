9a3ff442bc589bdd6f1b931061225442fbd02745
Julien Rousselot <julienrousselot16@gmail.com>
2024-05-02 20:13:43 +0200

    ajout derniere page mail/ possibilité d'envoi de mail ok

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,44 +1,6 @@
 const moonButton = document.getElementById("moonButton");
 const sunButton = document.getElementById("sunButton");
 
-sunButton.addEventListener("click", () => {
-  document.querySelectorAll(".day").forEach(element => {
-  element.style.display = "none";
-});
-
-document.querySelectorAll(".night").forEach(element => {
-  element.style.display = "flex";
-  });
-  document.querySelector('h1').classList.add('darkTheme');
-  document.querySelector('.topPage').classList.add('darkThemeThing');
-  document.querySelector('.midPageProject').classList.add('darkThemePurple');
-  document.querySelector('.navLien').classList.add('darkThemeThing');
-
-
-  document.querySelectorAll('.buttonIcon').forEach(btnElement => {
-    btnElement.classList.add('darkThemeThing');
-  });
-
-  document.querySelector('.textTopPage').classList.add('darkTheme');
-  document.querySelectorAll('h3').forEach(h3Element => {
-    h3Element.classList.add('darkThemeWriteBlack');
-  });
-  document.querySelector('.midPageSkills').classList.add('darkThemeSection');
-  document.querySelector('footer').classList.add('darkThemeSection');
-
-
-  document.querySelectorAll('.navImg').forEach((img, index) => {
-    if (index === 0) {
-      img.src = "../image/picturemyselfnight.svg";
-    } else if (index === 1) {
-      img.src = "../image/ordiclaviernight.svg";
-    } else if (index === 2) {
-      img.src = "../image/canapenight.svg";
-    }
-  });
-  
-});
-
 moonButton.addEventListener("click", () => {
   document.querySelectorAll(".night").forEach(element => {
     element.style.display = "none";
@@ -46,13 +8,11 @@ moonButton.addEventListener("click", () => {
   document.querySelectorAll(".day").forEach(element => {
     element.style.display = "flex";
   });
-  
   document.querySelector('h1').classList.remove('darkTheme');
   document.querySelector('.topPage').classList.remove('darkThemeThing');
   document.querySelector('.midPageProject').classList.remove('darkThemePurple');
   document.querySelector('.navLien').classList.remove('darkThemeThing');
-
-
+  document.querySelector('.btnTop').classList.remove('arrowButtonDark');
   document.querySelectorAll('.buttonIcon').forEach(btnElement => {
     btnElement.classList.remove('darkThemeThing');
   });
@@ -61,9 +21,7 @@ moonButton.addEventListener("click", () => {
     h3Element.classList.remove('darkThemeWriteBlack');
   });
   document.querySelector('.midPageSkills').classList.remove('darkThemeSection');
-  document.querySelector('footer').classList.remove('darkThemeSectionSection');
-
-
+  document.querySelector('footer').classList.remove('darkThemeSection');
   document.querySelectorAll('.navImg').forEach((img, index) => {
     if (index === 0) {
       img.src = "../image/picturemyself.svg";
@@ -74,3 +32,37 @@ moonButton.addEventListener("click", () => {
     }
   });
 });
+
+sunButton.addEventListener("click", () => {
+  document.querySelectorAll(".day").forEach(element => {
+  element.style.display = "none";
+});
+document.querySelectorAll(".night").forEach(element => {
+  element.style.display = "flex";
+  });
+  document.querySelector('h1').classList.add('darkTheme');
+  document.querySelector('.topPage').classList.add('darkThemeThing');
+  document.querySelector('.midPageProject').classList.add('darkThemePurple');
+  document.querySelector('.navLien').classList.add('darkThemeThing');
+  document.querySelector('.btnTop').classList.add('arrowButtonDark');
+  document.querySelectorAll('.buttonIcon').forEach(btnElement => {
+    btnElement.classList.add('darkThemeThing');
+  });
+  document.querySelector('.textTopPage').classList.add('darkTheme');
+  document.querySelectorAll('h3').forEach(h3Element => {
+    h3Element.classList.add('darkThemeWriteBlack');
+  });
+  document.querySelector('.midPageSkills').classList.add('darkThemeSection');
+  document.querySelector('footer').classList.add('darkThemeSection');
+  document.querySelectorAll('.navImg').forEach((img, index) => {
+    if (index === 0) {
+      img.src = "../image/picturemyselfnight.svg";
+    } else if (index === 1) {
+      img.src = "../image/ordiclaviernight.svg";
+    } else if (index === 2) {
+      img.src = "../image/canapenight.svg";
+    }
+  });
+});
+
+
